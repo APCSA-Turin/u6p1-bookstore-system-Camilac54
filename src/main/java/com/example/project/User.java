8f80444231dcadd93d50b36e0bf464e8305d50c8
Camilac54 <camilac54@nycstudents.net>
2025-01-14 17:02:01 +0000

    First Save 1/14/2025

--- src/main/java/com/example/project/User.java
+++ src/main/java/com/example/project/User.java
@@ -2,23 +2,47 @@ package com.example.project;
 
 public class User{
     //requires 3 private attributes String name, String Id, Book book that is initialized to empty
+    private String name; 
+    private String ID;
+    private Book book;
 
     //requires 1 contructor with two parameters that will initialize the name and id
- 
-    // public  getName() {}
+    public User (String name, String ID) {
+        this.name = name;
+        this.ID = ID;
+    }
 
-    // public  setName() {}
+    public String getName() {
+        return name;
+    }
 
-    // public  getId() {}
 
-    // public void setId() {}
+    public void setName(String newName) {
+        name = newName;
+    }
 
-    // public getBooks() {}
+    public String getId() {
+        return ID;
+    }
 
-    // public setBooks() {}
+    public void setId(String newID) {
+        ID = newID;
+    }
 
-    // public String bookListInfo(){} //returns a booklist for the user, if empty, output "empty"
+    public Book getBooks() {
+        return book;
+    }
 
-    // public String userInfo(){} //returns  "Name: []\nID: []\nBooks:\n[]"
+    public setBooks(Book newBook) {
+        book = newBook;
+    }
+
+    //public String bookListInfo(){ //returns a booklist for the user, if empty, output "empty"}
+
+
+    public String userInfo(){ //returns  "Name: []\nID: []\nBooks:\n[]"
+        String str =    "Name:" + name + "\nID: " + ID + "\nBooks:\n[]";
+        return str;
+    }
        
 }
